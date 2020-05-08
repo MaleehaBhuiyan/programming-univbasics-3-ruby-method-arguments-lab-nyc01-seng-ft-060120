0aa8ea3c426e47140783b9e7f624fb1c883f76c0
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-08 17:21:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,13 +8,12 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Josh")
 
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Edwin","Ruby")
+
 
 def introduction_with_language_optional(name,language="Ruby")
   puts "Hi. my name is #{name} and I am learning to program in #{language}."
